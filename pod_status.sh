c004bbf5f05c8dac49912a6c47b0d90fa3a11cb4
ranvirkardhanraj <54540641+ranvirkardhanraj@users.noreply.github.com>
2022-11-01 14:33:13 +0530

    Update pod_status.sh

--- pod_status.sh
+++ pod_status.sh
@@ -4,7 +4,7 @@ do
 	i=$(ssh root@192.168.30.161 kubectl get deployment --no-headers | awk '{print $2}')
 	if [ "$i" == "$b" ]
 	then
-		nohup ./portf.sh > portf-out.txt
+		nohup ./portf.sh > portf-out.txt &
 		echo "Port-forwarding started and you can access application now"
 		echo "Thank you for waiting"
 		break
