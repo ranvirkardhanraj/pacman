2c9beeb3f968da1ce728f0a8f9dfdedccace245a
ranvirkardhanraj <54540641+ranvirkardhanraj@users.noreply.github.com>
2022-11-01 15:58:13 +0530

    Update pod_status.sh

--- pod_status.sh
+++ pod_status.sh
@@ -1,12 +1,12 @@
-b=1/1
+b=0/1
 while true
 do
 	i=$(ssh root@192.168.30.161 kubectl get deployment --no-headers | awk '{print $2}')
 	if [ "$i" == "$b" ]
 	then
+		echo "Waiting for deployment ready"
+	else
 		ssh root@192.168.30.161 kubectl port-forward deployment/pacman --address 0.0.0.0 80:80 &
 		break
-	else
-		echo "Waiting for deployment ready"
 	fi
 done
