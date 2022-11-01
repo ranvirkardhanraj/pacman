08a0b682814561ade5f8ac619feb157a879beaad
ranvirkardhanraj <54540641+ranvirkardhanraj@users.noreply.github.com>
2022-11-01 14:55:55 +0530

    Update pod_status.sh

--- pod_status.sh
+++ pod_status.sh
@@ -4,9 +4,9 @@ do
 	i=$(ssh root@192.168.30.161 kubectl get deployment --no-headers | awk '{print $2}')
 	if [ "$i" == "$b" ]
 	then
-		nohup ./portf.sh > portf-out.txt &
 		echo "Port-forwarding started and you can access application now"
 		echo "Thank you for waiting"
+		ssh root@192.168.30.161 kubectl port-forward deployment/pacman --address 0.0.0.0 80:80 &
 		break
 	else
 		echo "Waiting for deployment ready"
