1b1a6f4cccb86d1f197265e5bf89fc1d0bc7255b
ranvirkardhanraj <54540641+ranvirkardhanraj@users.noreply.github.com>
2022-11-01 11:41:48 +0530

    Update pod_status.sh

--- pod_status.sh
+++ pod_status.sh
@@ -1,9 +1,8 @@
-b="1/1"
+b=1/1
 while true
-#for i in $(ssh root@192.168.30.161 kubectl get deployment --no-headers | awk '{print $2}')
 do
 	i=$(ssh root@192.168.30.161 kubectl get deployment --no-headers | awk '{print $2}')
-	if [ $i == $b ]
+	if [ "$i" == "$b" ]
 	then
 		ssh root@192.168.30.161 kubectl port-forward deployment/pacman --address 0.0.0.0 80:80 &
 		break
