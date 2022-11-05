271cb35dd59b93b61b95ed1bc6323b6989598486
ranvirkardhanraj <54540641+ranvirkardhanraj@users.noreply.github.com>
2022-11-05 10:46:05 +0530

    Update pod_status.sh

--- pod_status.sh
+++ pod_status.sh
@@ -7,7 +7,7 @@ do
 		echo "Waiting for deployment ready"
 	else
 		#ssh root@192.168.30.161 kubectl port-forward deployment/pacman --address 0.0.0.0 80:80 &
-		ssh root@192.168.30.161 kubectl expose deployment pacman --type=NodePort --target-port=80 --port=80
+		ssh root@192.168.30.161 kubectl expose deployment pacman --type=LoadBalancer --target-port=80 --port=80
 		break
 	fi
 done
